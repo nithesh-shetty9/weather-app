bea610103aa2a6d435a0e467fcd031bb2fa154d4
Nithesh <nitheshnithesh310@gmail.com>
2026-06-21 15:50:48 +0530

    Add temperature and weather description elements with styling updates

---
+++ script.js
@@ -0,0 +1,6 @@
+let temp=document.querySelector(".temp");
+let description=document.querySelector(".desc");
+let humidity=document.querySelector(".humdity");
+let wind=document.querySelector(".wind");
+let city=document.querySelector(".city");
+let icon=document.querySelector(".icon");
